708634bb9d11a66b8b34de0f553a36ce5b1c5f95
itzVort3x1 <saikaustubh10@gmail.com>
2023-03-22 13:48:44 +0530

    updated sandbox

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,20 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car', {
+		name: 'Ferrari',
+		color: 'red',
+		year: 1950
+	});
+
+	const car = await client.hGetAll('car');
+
+	if (Object.keys(car).length === 0) {
+		console.log('Car not found, respond with 404');
+		return;
+	}
+
+	console.log(car);
+};
 run();
