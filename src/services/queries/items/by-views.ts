5d0cb3a4e2ef587ad9c0a62a822c829e1b7ea701
itzVort3x1 <saikaustubh10@gmail.com>
2023-03-28 07:08:52 +0530

    added arguments to sort

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -4,7 +4,12 @@ import { itemsByViewsKey, itemsKey } from '$services/keys';
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
 	const results = await client.sort(itemsByViewsKey(), {
 		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
-		BY: 'nosort'
+		BY: 'nosort',
+		DIRECTION: order,
+		LIMIT: {
+			offset,
+			count
+		}
 	});
 
 	console.log(results);
