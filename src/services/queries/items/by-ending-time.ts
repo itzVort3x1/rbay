43c68839516dafe4945eba1e0e0a0676c7f4a6d3
itzVort3x1 <saikaustubh10@gmail.com>
2023-03-28 07:05:59 +0530

    added sorting criteria for itemsByViews

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -8,5 +8,11 @@ export const itemsByEndingTime = async (order: 'DESC' | 'ASC' = 'DESC', offset =
 		LIMIT: { offset, count }
 	});
 
-	console.log(ids);
+	const results = await Promise.all(
+		ids.map((id) => {
+			return client.hGetAll(itemsKey(id));
+		})
+	);
+
+	return results.map((item, i) => deserialize(ids[i], item));
 };
